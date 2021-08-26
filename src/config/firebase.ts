74fb3d03373f1255d4cdbe2c476d3b48c7a1e1b2
Julian Barragan Verano <julian.barragan@adidas.com>
2021-08-26 14:55:17 -0500

    Upload files to firebase storage

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,20 +1,28 @@
 import firebase from 'firebase/app'
 import 'firebase/analytics'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCrUTGPr34b_pOFbVAavE7ieZJ3jeUDL9k',
-  authDomain: 'yamile-julian.firebaseapp.com',
-  databaseURL: 'https://yamile-julian-default-rtdb.firebaseio.com',
-  projectId: 'yamile-julian',
-  storageBucket: 'yamile-julian.appspot.com',
-  messagingSenderId: '65183147064',
-  appId: '1:65183147064:web:1fa4b8173d86148d88f2cd',
-  measurementId: 'G-DLKY9YQMFJ',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_API_KEY,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig)
 firebase.analytics()
+
+firebase
+  .auth()
+  .signInWithEmailAndPassword(
+    process.env.FIREBASE_EMAIL || '',
+    process.env.FIREBASE_PASS || ''
+  )
 
 export const db = fb.firestore()
 export const firestore = firebase.firestore
